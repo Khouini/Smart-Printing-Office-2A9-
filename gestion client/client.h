7c368c9610559f91cb29581cf5a7eba92bc5dac2
Wahch <mohamed.wahchi@esprit.tn>
2022-03-03 14:37:05 +0100

    v0.1
    crud

---
+++ gestion client/client.h
@@ -0,0 +1,41 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+#include<QString>
+#include<QSqlQuery>
+#include<QSqlQueryModel>
+
+
+class client
+{
+    QString   nom , prenom , email , sexe , type_client ;
+    int idclient , cinn ;
+public:
+
+    //constructeurs
+    client();
+    client(QString,QString,QString,QString,QString,int,int) ;
+    /*//get
+    int getidclient(){return idclient;}
+    int getcinn(){return cinn;}
+    QString getnom(){return nom;}
+    QString getprenom(){return prenom;}
+    QString getemail(){return email;}
+    QString getsexe(){return sexe;}
+    QString gettype_client(){return type_client;}
+    //set
+    void setidclient(int idclient){this->idclient=idclient;}
+    void setcinn(int cinn){this->cinn=cinn;}
+    void setnom(QString nom){this->nom=nom;}
+    void setprenom(QString prenom){this->prenom=prenom;}
+    void setemail(QString email){this->email=email;}
+    void setsexe(QString sexe){this->sexe=sexe;}
+    void settype_client(QString type_client){this->type_client=type_client;}*/
+    //fonction
+    bool ajouterclient() ;
+    QSqlQueryModel * afficher() ;
+    bool supprimer(int) ;
+    bool modifierclient() ;
+
+};
+
+#endif // CLIENT_H
