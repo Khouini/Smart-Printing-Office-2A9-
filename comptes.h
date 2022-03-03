01431061db75708e6a467b4491b2e20a6d0d7321
Khouini <medyacine.khouini@esprit.tn>
2022-03-03 14:04:38 +0100

    v0.1
    crud completed

---
+++ comptes.h
@@ -0,0 +1,21 @@
+#ifndef COMPTES_H
+#define COMPTES_H
+#include <QString>
+#include <QSqlQuery>
+#include <QSqlQueryModel>
+
+class Comptes
+{
+    int Numero;
+    QString Nom, Classe, Type ,  Caract;
+public:
+    Comptes();
+    Comptes(int, QString, QString, QString,  QString);
+    bool ajouter();
+    QSqlQueryModel * afficher();
+    bool supprimer(int);
+    bool modifier();
+    QSqlQueryModel * trier(QString type);
+};
+
+#endif // COMPTES_H
